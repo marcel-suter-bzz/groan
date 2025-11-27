aeeb0c98f5d22fdffec28bf72876e873e0df158a
Marcel Suter <marcel.suter@bzz.ch>
2025-11-27 07:53:54 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+
+def main():
+    show_welcome()
+    total_rounds = throw_dice(2)
+    print(f'Wir spielen {total_rounds} Runden')
+    round = 1
+    while (round < total_rounds ):
+        round+=1
+
+
+def throw_dice(number):
+    i = 0
+    dice = list()
+    while i < number:
+        dice.append(random.randint(1,6))
+        i+=1
+
+    return dice
+
+def show_welcome():
+    """
+    shows the welcome message
+    :return:
+    """
+    print('Willkommen zu Groan! Ein taktisches Würfelspiel für 2 Spieler')
+    choice = input('Willst du die Regeln lesen (y/N)')
+    if choice.lower() == 'y':
+        print ('Regeln blabla')
+
+
+
+
+if __name__ == '__main__':
+    main()
